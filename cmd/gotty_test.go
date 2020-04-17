2b071e384239435cfb0b08fc4ecd5c95e1713f7d
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-17 14:15:58 +0200

    scaffolding for switching to cobra

--- cmd/gotty_test.go
+++ cmd/gotty_test.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func Test_ExecuteCommand(t *testing.T) {
+	cmd := NewRootCmd("hi")
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+	cmd.Execute()
+	out, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(out) != "hi" {
+		t.Fatalf("expected \"%s\" got \"%s\"", "hi", string(out))
+	}
+}
